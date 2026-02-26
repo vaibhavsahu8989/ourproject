638904edbe3160f2b2b417a3e7c74f9f0eb895d4
vaibhavsahu8989 <vaibhavsahu8989@gmail.com>
2026-02-26 15:03:36 +0530

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    fs: false,
+    path: false,
+    util: false
+  };
+  return config;
+};
